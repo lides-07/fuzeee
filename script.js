095c06c96d24170af75792615015eb3930cd506c
yassine <yassinelamachi16@gmail.com>
2024-12-21 21:49:24 +0100

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-document.getElementById("surpriseButton").addEventListener("click", function () {
-    const surprise = document.getElementById("surprise");
-    surprise.classList.toggle("hidden");
-    if (!surprise.classList.contains("hidden")) {
-        alert("Fuzee, this is for you. I hope it brightens your day! ❤️");
-    }
-});
